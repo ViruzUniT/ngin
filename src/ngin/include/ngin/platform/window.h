d5f27e17a9d96a6cf374cc6d574a9e2b4331d64c
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2026-06-05 08:55:34 +0200

    ref: move hwnd to window prop obj

--- src/ngin/include/ngin/platform/window.h
+++ src/ngin/include/ngin/platform/window.h
@@ -1,23 +1,26 @@
 #pragma once
 #include <ngin/core/base.h>
+#include <ngin/pch.h>
 #include <stdint.h>
 
 namespace Ngin {
 namespace Window {
-
 enum CmdShow {
   Hide = 0,
-  ShowNormal,
+  ShowNormal = 1,
   Show = 5,
 };
 
 struct Properties {
   uint16_t width;
   uint16_t height;
-  std::string_view name;
+  std::string name;
+  std::string className = "NginWindow";
+  HWND hwnd = nullptr;
 
-  Properties(uint16_t width, uint16_t height, std::string_view name)
-      : width(width), height(height), name(name) {}
+  Properties(uint16_t width, uint16_t height, std::string_view name, std::string_view className)
+      : width(width), height(height), name(name), className(className) {}
+  Properties() = default;
 };
 
 ErrorCode Create(Properties const& props);
