bb8e2b1e902e6ce65290f2f9cb9d65f3678adeed
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2026-06-23 16:45:11 +0200

    feat: add renderer

---
+++ src/ngin/include/ngin/renderer/renderer.h
@@ -0,0 +1,6 @@
+#include <ngin/pch.h>
+namespace Renderer {
+void Initialize(HWND handle, uint16_t width, uint16_t height);
+void RenderFrame();
+void Resize(uint16_t width, uint16_t height);
+}  // namespace Renderer
