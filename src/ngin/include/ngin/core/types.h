714deff5948bae526c61a7b055fbc50af5c67972
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2026-06-29 15:55:46 +0200

    fix: add custom deleter for com rhi components

--- src/ngin/include/ngin/core/types.h
+++ src/ngin/include/ngin/core/types.h
@@ -3,12 +3,24 @@
 #include <cstddef>
 #include <memory>
 
+struct ComReleaser {
+  template <typename T>
+  void operator()(T* ptr) const {
+    if (ptr) {
+      ptr->Release();
+    }
+  }
+};
+
 namespace Ngin {
 using f32 = float;
 using f64 = double;
 
 template <typename T>
 using Scope = std::unique_ptr<T>;
+template <typename T>
+using ComScope = std::unique_ptr<T, ComReleaser>;
+
 template <typename T>
 using Ref = std::shared_ptr<T>;
 
