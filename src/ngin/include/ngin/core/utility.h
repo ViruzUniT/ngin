43632ce73e62c3cdeb1b770c28e77e017ecbda95
ViruzUniT <73714544+ViruzUniT@users.noreply.github.com>
2026-09-07 08:30:25 +0200

    cleanup: remove scopeexit struct

--- src/ngin/include/ngin/core/utility.h
+++ src/ngin/include/ngin/core/utility.h
@@ -39,40 +39,4 @@ class NonCopyableNonMovable {
   NonCopyableNonMovable(NonCopyableNonMovable&&) = delete;
   NonCopyableNonMovable& operator=(NonCopyableNonMovable&&) = delete;
 };
-
-template <typename Function>
-class ScopeExit {
- public:
-  explicit ScopeExit(Function function) noexcept(std::is_nothrow_move_constructible_v<Function>)
-      : function_(std::move(function)) {}
-
-  ScopeExit(ScopeExit&& other) noexcept(std::is_nothrow_move_constructible_v<Function>)
-      : function_(std::move(other.function_)), active_(std::exchange(other.active_, false)) {}
-
-  ~ScopeExit() noexcept(noexcept(std::declval<Function&>()())) {
-    if (active_) {
-      function_();
-    }
-  }
-
-  ScopeExit(const ScopeExit&) = delete;
-  ScopeExit& operator=(const ScopeExit&) = delete;
-  ScopeExit& operator=(ScopeExit&&) = delete;
-
-  void release() noexcept { active_ = false; }
-
- private:
-  Function function_;
-  bool active_ = true;
-};
-
-template <typename Function>
-ScopeExit(Function) -> ScopeExit<Function>;
-
-template <typename Enum>
-constexpr std::underlying_type_t<Enum> toUnderlying(Enum value) noexcept {
-  static_assert(std::is_enum_v<Enum>, "toUnderlying requires an enum type.");
-  return static_cast<std::underlying_type_t<Enum>>(value);
-}
-
 }  // namespace Ngin
